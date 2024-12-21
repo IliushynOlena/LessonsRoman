973118f81cdd4a02512f1d28809a47d87c3323f4
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-12-21 14:59:04 +0200

    String, String Builder

---
+++ 06_StringBuilder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace _06_StringBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello";
+            foreach (char letter in str)
+            {
+                if (Char.IsUpper(letter))
+                    countletter++;
+            }
+        
+            str += "bla";
+            str += "bla";
+            str += "bla";
+            str += "bla";
+            str += "bla";
+            str += "bla";
+            str += "bla";
+            str += "bla";
+            Console.WriteLine(str);
+
+            StringBuilder builder = new StringBuilder();
+            Console.WriteLine($"String lenght : {builder.Length}");
+            Console.WriteLine($"String capacity : {builder.Capacity}");
+            builder.Append("Hello");
+            builder.Append("Hello");
+            builder.Append("Hello");
+            Console.WriteLine($"String lenght : {builder.Length}");
+            Console.WriteLine($"String capacity : {builder.Capacity}");
+            builder.AppendLine("Hello");
+            builder.Append("Hello");
+            builder.Append("Hello");
+            Console.WriteLine($"String lenght : {builder.Length}");
+            Console.WriteLine($"String capacity : {builder.Capacity}");
+            builder.Append("Hello");
+            builder.Append("Hello");
+            builder.Append("Hello");
+            Console.WriteLine($"String lenght : {builder.Length}");
+            Console.WriteLine($"String capacity : {builder.Capacity}");
+            Console.WriteLine(builder);
+
+        }
+    }
+}
